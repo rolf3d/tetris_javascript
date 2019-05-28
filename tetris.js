1faebed97fa1a173f30f791e2a4cda5bb26bdb69
Rolf Petersen <rolf3d@gmail.com>
2019-05-28 19:14:40 +0200

    All figures

--- tetris.js
+++ tetris.js
@@ -4,13 +4,6 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 
-
-const matrix = [
-    [0,0,0],
-    [1,1,1],
-    [0,1,0],
-];
-
 function collide(arena, player){
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y){
@@ -31,6 +24,58 @@ function createMatrix(w, h){
         matrix.push(new Array(w).fill(0));
     }
     return matrix;
+}
+
+function createPiece(type){
+    if (type === 'T'){
+        return [
+            [0,0,0],
+            [1,1,1],
+            [0,1,0],
+        ];
+    }
+    else if(type === 'O'){
+        return [
+            [1,1],
+            [1,1],
+        ];
+    }
+    else if(type === 'L'){
+        return [
+            [0,1,0],
+            [0,1,0],
+            [0,1,1],
+        ];
+    }
+    else if(type === 'J'){
+        return [
+            [0,1,0],
+            [0,1,0],
+            [1,1,0],
+        ];
+    }
+    else if(type === 'I'){
+        return [
+            [0,1,0,0],
+            [0,1,0,0],
+            [0,1,0,0],
+            [0,1,0,0],
+        ];
+    }
+    else if(type === 'S'){
+        return [
+            [0,1,1],
+            [1,1,0],
+            [0,0,0],
+        ];
+    }
+    else if(type === 'Z'){
+        return [
+            [1,1,0],
+            [0,1,1],
+            [0,0,0],
+        ];
+    }
 }
 
 function draw(){
@@ -68,7 +113,8 @@ function playerDrop(){
     if (collide(arena, player)){
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
+        
     }
     dropCounter = 0;
 }
@@ -80,8 +126,30 @@ function playerMove(dir){
     }
 }
 
+function playerReset(){
+    const pieces = 'ILJOTSZ';
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) - 
+    (player.matrix[0].length / 2 | 0);
+    if(collide(arena, player)){
+        arena.forEach(row => row.fill(0));
+    }
+}
+
 function playerRotate(dir){
+    const pos = player.pos.x;
+    let offset = 1;
     rotate(player.matrix, dir);
+    while(collide(arena, player)){
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if(offset > player.matrix[0].length){
+            rotate(player.matrix, - dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
 }
 
 function rotate(matrix, dir){
@@ -123,7 +191,7 @@ const arena = createMatrix(12, 20);
 
 const player = {
     pos:{x: 5, y: 5},
-    matrix: matrix,
+    matrix: createPiece('T'),
 }
 
 document.addEventListener('keydown', event => {
